26daa94d8ec550dd49e1ebfd23c38446897ad4a4
EGAguirre <9eaguirre@stpatrick.org>
2017-03-22 11:27:07 -0500

    Add files via upload

--- click.js
+++ click.js
@@ -1,10 +1,17 @@
 //declare samecolor() here
-function samecolor(hex){
+function samecolor(){
+  let color = "#A4FFE7"
   const doc = document;
 
-  let divs = doc.querySelectorAll("#Samecolor div")
-   
+  let divs = doc.querySelectorAll("#Samecolor div");
+  for(){
+    divs[i].style.backgroundColor =
+  }
+
 
 
 }
 //declare diffcolor() here
+function diffcolor(){
+
+}
