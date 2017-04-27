73d2153ad1e83303414cc3f54f5bf563d820bde4
EGAguirre <9eaguirre@stpatrick.org>
2017-04-27 10:46:59 -0500

    Add files via upload

--- resize.js
+++ resize.js
@@ -1,4 +1,10 @@
 //Declare tabletSize() here
-
+function tabletSize(){
+  const doc = document;
+  let sections = doc.querySelectorAll();
+}
 
 //Declare phoneSize() here
+function phoneSize(){
+
+}
